f02da1536539893ca08100afc66ca7c916af3570
XiaoPaozhang <2782225452@qq.com>
2026-06-07 08:27:13 +0800

    优化异常学习代码

--- Assets/Xpzhnl/Exception/ExceptionTest.cs
+++ Assets/Xpzhnl/Exception/ExceptionTest.cs
@@ -7,30 +7,33 @@ namespace LearnUnity
 {
   public class ExceptionTest : MonoBehaviour
   {
+    Dog dog = null;
+    string str = string.Empty;
+
     [ContextMenu("test")]
     void Start()
     {
-        var ints = new List<int>();
-        A obja = null;
-        ints.Add(1);
+        // ---unity报错不会【打断】整个函数体后续代码执行
         Debug.LogError("错误");
-        Debug.Log(ints[0]);
+
+        // ---这里dog是空，直接拿name会报空指针异常
+        // ---此异常是c#语法异常，会【打断】整个函数体内后续代码执行
+        // str = dog.name;
+
+        // ---更换为下方try-catch的方式
+        // ---可以在出现异常时，不【打断】后续代码，并执行catch体里的代码
         try{
-            obja = obja.a == 0 ? new A() : null;
+            str = dog.name;
         }catch(Exception e){
             Debug.LogError("异常："+e);
         }
-        Debug.Log(obja);
-    }
 
-    void Update()
-    {
-      
+        Debug.Log("我被打印了，证明前面的异常没有打断我");
     }
-  }
-
-  public class A {
-    public int a;
+    
+    class Dog {
+        public string name;
+    }
   }
 }
 
